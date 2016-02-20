68c8260af2ba65f3345e647b913c40e39c01c554
MuuM <724024307@qq.com>
2016-02-20 14:55:59 +0800

    1

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    public float rotate_sped;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(new Vector3(0, 0, rotate_sped * Time.deltaTime));
+	}
+}
